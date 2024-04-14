b42ca884ffcfe965b942bf8c8c1f20d71e6bed2f
Ye Yan Naing <yeyannaing85@gmail.com>
2024-04-14 19:58:36 +0630

    [Complete]Ado.net CURD Day1

--- DotNetTrainingBatch4.ConsoleApp/Program.cs
+++ DotNetTrainingBatch4.ConsoleApp/Program.cs
@@ -1,23 +1,23 @@
-﻿using System.Data.SqlClient;
-Console.WriteLine("Hello, World!");
+﻿using DotNetTrainingBatch4.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
 
+Console.WriteLine("Hello, World");
 
+// npm
+// pub.dev
+// nuget
+// SqlConnection
 
-
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "HR";
-stringBuilder.InitialCatalog = "DotNetTrainingBatch4";//database name
-stringBuilder.UserID = "sa";//username
-stringBuilder.Password = "sa@123";//password
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-connection.Open();
-Console.WriteLine("Connection Open");
-string query = "select * from tbl_blog";
-SqlCommand cmd= new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-
-connection.Close();
-Console.WriteLine("Connection Close");
+// Ctrl + .
+// C# => Db
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(3, "test title", "test author", "test content");
+//adoDotNetExample.Delete(3);
+adoDotNetExample.Edit(2);
+// Ado.net 
+// CRUD
 
 Console.ReadKey();
-
